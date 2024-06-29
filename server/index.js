39087ce85b91165cfab2619a84c4ee7d04d15b92
muzayenadem <muzynadem@gmail.com>
2024-06-30 01:01:50 +0300

    for test

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const app = express()
 app.get('/',(req,res)=>{
     res.status(200).send('Hello wrold')
 })
+app.get('/rahmaan',(req,res)=>{
+    res.send('yaa rabbi naaf laaffisi waan hundaa')
+})
 
 app.listen(4444,()=>{
     console.log('susseed')
