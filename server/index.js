0c5c46e247b3dd70991fc4727f23752c08d746eb
muzayenadem <124003376+muzayenadem@users.noreply.github.com>
2026-07-26 17:00:38 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,58 @@
-
+const  {v2} =  require('cloudinary')
 const cors = require('cors')
 const express = require('express');
 const bodyParser = require('body-parser');
+const brevo = require('@getbrevo/brevo');
+const apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(brevo.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_EMAIL_SENDER_API);
 
 const app = express();
 //const PORT = process.env.PORT || 3000;
 const PORT = 4000
+
+
+
+// for sending message
+
+let cloudinary = v2
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+const sendWelcomeEmail = async (email) => {
+  try {
+    await apiInstance.sendTransacEmail({
+      subject: "You've Joined Successfully!",
+      sender: { 
+        email: "atdodola@gmail.com", 
+        name: "Next Generation Software Company" 
+      },
+      to: [{ email }],
+      htmlContent: `
+        <h1>Welcome!</h1>
+        <p>You've successfully subscribed to our service.</p>
+        <a href="https://dodola-official-website.vercel.app">Get started now!</a>
+      `,
+      textContent: "Thanks for joining us!",
+    });
+    console.log(`Email sent to ${email}`);
+  } catch (error) {
+    console.error("Brevo Error:", error.response?.body || error.message);
+    throw error;
+  }
+};
+
+
+function assureEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const gmailDomain = /@gmail\.com$/;
+    return emailRegex.test(email) && gmailDomain.test(email);
+}
+
+
+
 
 // Middleware to parse JSON data
 app.use(bodyParser.json());
@@ -13,6 +60,16 @@ app.use(cors())
 app.post('/send-email', (req, res) => {
     try {
         const { name, phone, email, subject, message } = req.body;
+        const assure_email = assureEmail(email)
+        if(!assure_email):
+            return res.status(401).json({"error":"invalid email"})
+
+        try{
+            await sendWelcomeEmail(email)
+            return res.status(200).send("email sent successfully")  
+        }
+        catch (error){
+        }
         res.status(200).send('Your message has been sent successfully')
     } catch (error) {
         res.status(500).send(error.message)
