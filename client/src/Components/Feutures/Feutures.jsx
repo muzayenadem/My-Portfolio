4a437cc27db0280417ca50655895beb780c04b79
muzayenadem <124003376+muzayenadem@users.noreply.github.com>
2026-07-30 14:34:05 +0300

    Update Feutures.jsx

--- client/src/Components/Feutures/Feutures.jsx
+++ client/src/Components/Feutures/Feutures.jsx
@@ -6,7 +6,7 @@ function Feutures() {
   return (
     <section id='Feutures' className='w-[94%] py-20 h-auto  mx-auto border-b-[1px] border-b-black' >
         <Title title='Features' des='What i do'/>
-       <div className='grid px-3 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-20'>
+       <div className='grid px-3 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5'>
         <Card/>
        </div>
         </section>
