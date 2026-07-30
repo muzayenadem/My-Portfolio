151867d97797c23a34231798a98fda9bb03ce501
muzayenadem <124003376+muzayenadem@users.noreply.github.com>
2026-07-30 14:05:08 +0300

    Update ProjectCard.jsx

--- client/src/Components/Projects/ProjectCard.jsx
+++ client/src/Components/Projects/ProjectCard.jsx
@@ -6,7 +6,7 @@ function ProjectCard() {
   return (
     <>
     {
-        projects.map(({title,des,img,webLink,gitgubLink},i) =>{
+        projects.map(({title,des,img,webLink,githubLink},i) =>{
             return (
                 <div 
                 className='w-full  h-auto px-12 py-10 rounded-lg shadow-md shadow-gray-700 flex flex-col items-center bg-gradient-to-r from-neutral-900 to-stone-800
@@ -19,7 +19,7 @@ function ProjectCard() {
                         <div className='flex items-center justify-between'>
                           <h3 className='text-xl font-bold uppercase text-rose-600'>{title}</h3>
                             <div className='flex justify-between'>
-                                <a href={gitgubLink}>
+                                <a href={githubLink}>
                                 <button className='text-lg w-10 h-10 bg-black rounded-full text-slate-300  inline-flex justify-center  items-center hover:text-rose-800 duration-300'>
                                 <FaGithub/>
                             </button>
@@ -51,21 +51,21 @@ const projects = [
         des:"A MERN application that allows landlords to list properties and users to search rentals with interactive maps.",
         img:img5,
         webLink:'https://gofind.vercel.app',
-        gitgubLink:'https://github.com/muzayenadem/GoFind'
+        githubLink:'https://github.com/muzayenadem/GoFind'
     },
     {
         title:'Dodola Official Website',
         des:"A full-stack community website with an admin dashboard for managing news, events, announcements, and public content.",
         img:"https://res.cloudinary.com/dtmtgaepv/image/upload/v1785405245/News_Image/teiihrvmcp5eyupssx01.jpg",
         webLink:'https://dodola-official-websitq¡.vercel.app',
-        gitgubLink:'https://github.com/muzayenadem/Ecommerce'
+        githubLink:'https://github.com/muzayenadem/Ecommerce'
     }, 
     {
         title:"House Price Predictor",
         des: "A machine learning model that predicts house prices using data preprocessing and neural network techniques",
         img:img5,
         webLink:'https://gofind.vercel.app',
-        gitgubLink:'https://github.com/muzayenadem/GoFind'
+        githubLink:'https://github.com/muzayenadem/GoFind'
     },
     {
         title:'E-Commerce Demo',
@@ -79,13 +79,13 @@ const projects = [
         des:'A responsive portfolio showcasing my projects, skills, and experience with a functional contact form.',
         img:img4,
         webLink:'https://gofind.vercel.app',
-        gitgubLink:'https://github.com/muzayenadem/GoFind'
+        githubLink:'https://github.com/muzayenadem/GoFind'
     },
     {
         title:'Diary Application',
         des:"A modern diary application where users can write, organize, and manage personal notes securely.",
         img:img6,
         webLink:'https://gofind.vercel.app',
-        gitgubLink:'https://github.com/muzayenadem/GoFind'
+        githubLink:'https://github.com/muzayenadem/GoFind'
     },
 ]
